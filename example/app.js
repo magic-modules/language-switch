d8f86982bdf456dd97eba3751f0a8a520ea5edb0
Jascha <jascha@jaeh.at>
2019-04-30 02:58:44 +0200

    example: update menu and title

--- example/app.js
+++ example/app.js
@@ -1,16 +1,16 @@
 module.exports = {
   state: {
-    logo: '/img/logo.png',
-    logotext: 'pre',
+    logotext: 'languageswitch',
     languages: [
       { code: 'en', to: '/', text: 'english' },
       { code: 'de', to: '/de/', text: 'deutsch' },
     ],
     menu: [
-      { to: '/#installation', text: 'installation' },
-      { to: '/#require', text: 'require' },
-      { to: '/#usage', text: 'usage' },
-      { to: '/#fixed-color', text: 'fixed color' },
+      { to: '/#installation', text: 'installation:' },
+      { to: '/#require', text: 'require:' },
+      { to: '/#usage', text: 'usage:' },
+      { to: '/#pages', text: 'language pages' },
+      { to: '/#state', text: 'required state' },
       { to: '/#source', text: 'source' },
     ],
   },
