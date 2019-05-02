9e9bca32c9972887ca08b91a4a60649a17f92679
Jascha <jascha@jaeh.at>
2019-05-03 01:19:29 +0200

    update example pages

--- example/pages/de/index.js
+++ example/pages/de/index.js
@@ -1,59 +1,55 @@
-const state = {
-  lang: 'de',
-}
+const page = () => [
+  LanguageSwitch,
+  h1('@magic-modules/languageswitch'),
+  p([
+    'dies ist das ',
+    Link({ to: 'https://github.com/magic-modules' }, '@magic-modules'),
+    ' LanguageSwitch modul. Es rendert ein Menu mit Sprachauswahl.',
+  ]),
 
-const View = () =>
-    div([
-      LanguageSwitch,
-      h1('@magic-modules/languageswitch'),
-      p([
-        'dies ist das ',
-        Link({ to: 'https://github.com/magic-modules' }, '@magic-modules'),
-        ' LanguageSwitch modul. Es rendert ein Menu mit Sprachauswahl.',
-      ]),
+  GitBadges({
+    project: 'magic-modules/languageswitch',
+    appveyor: 'jaeh/languageswitch',
+  }),
 
-      GitBadges({
-        project: 'magic-modules/languageswitch',
-        appveyor: 'jaeh/languageswitch',
-      }),
+  h2({ id: 'installation' }, 'installation:'),
+  p([
+    'installation funktioniert durch npm.',
+    'in naher zukunft leben alle magic module auf github und nicht npm.',
+  ]),
+  p('das fehlende @ vor magic-modules ist, wie wir npm module von github installieren können.'),
 
-      h2({ id: 'installation' }, 'installation:'),
-      p([
-        'installation funktioniert durch npm.',
-        'in naher zukunft leben alle magic module auf github und nicht npm.',
-      ]),
-      p('das fehlende @ vor magic-modules ist, wie wir npm module von github installieren können.'),
-
-      Pre.View(`
+  Pre.View(`
 npm install magic-modules/languageswitch
 `),
 
-      h2({ id: 'require' }, 'importieren'),
-      p('zuerst laden wir das modul via /assets/index.js'),
-      Pre.View(`
+  h2({ id: 'require' }, 'importieren'),
+  p('zuerst laden wir das modul via /assets/index.js'),
+  Pre.View(`
 // assets/index.js
 module.exports = {
   //...other exports
   LanguageSwitch: require('@magic-modules/languageswitch'),
 }`),
-      h2({ id: 'usage' }, 'usage:'),
-      p('in einer page oder einem komponent'),
-      Pre.View("module.exports = {\n  View: () => div({ class: 'page' }, LanguageSwitch)\n}"),
+  h2({ id: 'usage' }, 'usage:'),
+  p('in einer page oder einem komponent'),
+  Pre.View("module.exports = {\n  View: () => div({ class: 'page' }, LanguageSwitch)\n}"),
 
-      h2({ id: 'source' }, 'source'),
-      p([
-        'der source dieser page ist im ',
-        Link(
-          { to: 'https://github.com/magic-modules/languageswitch/tree/master/example' },
-          'example directory',
-        ),
-        ' und wird mit ',
-        Link({ to: 'https://github.com/magic/core' }, '@magic/core'),
-        ' gebaut und zu github publiziert.',
-      ]),
-    ])
+  h2({ id: 'source' }, 'source'),
+  p([
+    'der source dieser page ist im ',
+    Link(
+      { to: 'https://github.com/magic-modules/languageswitch/tree/master/example' },
+      'example directory',
+    ),
+    ' und wird mit ',
+    Link({ to: 'https://github.com/magic/core' }, '@magic/core'),
+    ' gebaut und zu github publiziert.',
+  ]),
+]
 
-module.exports = {
-  state,
-  View,
+page.state = {
+  lang: 'de',
 }
+
+module.exports = page
--- example/pages/index.js
+++ example/pages/index.js
@@ -1,54 +1,52 @@
-module.exports = {
-  View: () =>
-    div([
-      LanguageSwitch,
-      h1('@magic-modules/languageswitch'),
-      p([
-        'this is the ',
-        Link({ to: 'https://github.com/magic-modules' }, '@magic-modules'),
-        ' LanguageSwitch component. It provides a language switch menu.',
-      ]),
+module.exports = () => [
+  LanguageSwitch,
+  h1('@magic-modules/languageswitch'),
+  p([
+    'this is the ',
+    Link({ to: 'https://github.com/magic-modules' }, '@magic-modules'),
+    ' LanguageSwitch component. It provides a language switch menu.',
+  ]),
 
-      GitBadges({
-        project: 'magic-modules/languageswitch',
-        appveyor: 'jaeh/languageswitch',
-      }),
+  GitBadges({
+    project: 'magic-modules/languageswitch',
+    appveyor: 'jaeh/languageswitch',
+  }),
 
-      h2({ id: 'installation' }, 'installation:'),
-      p(
-        'installation is done using npm. for now, all magic modules are living on github and not on npm.',
-      ),
-      p('note the missing @ before magic-modules.'),
-      p('this is how we install npm modules from github.'),
+  h2({ id: 'installation' }, 'installation:'),
+  p(
+    'installation is done using npm. for now, all magic modules are living on github and not on npm.',
+  ),
+  p('note the missing @ before magic-modules.'),
+  p('this is how we install npm modules from github.'),
 
-      Pre.View(`
+  Pre.View(`
 npm install magic-modules/languageswitch
 `),
 
-      h2({ id: 'require' }, 'require:'),
-      p('first add the component to the assets'),
-      Pre.View(`
+  h2({ id: 'require' }, 'require:'),
+  p('first add the component to the assets'),
+  Pre.View(`
 // assets/index.js
 module.exports = {
   //...other exports
   LanguageSwitch: require('@magic-modules/languageswitch'),
 }`),
-      h2({ id: 'usage' }, 'usage:'),
-      p('in a page or component'),
-      Pre.View('module.exports = {\n  View: () => LanguageSwitch,\n}'),
+  h2({ id: 'usage' }, 'usage:'),
+  p('in a page or component'),
+  Pre.View('module.exports = {\n  View: () => LanguageSwitch,\n}'),
 
-      h2({ id: 'pages' }, 'language pages'),
-      p('create your languages in the pages directory, for example:'),
-      Pre.View(`
+  h2({ id: 'pages' }, 'language pages'),
+  p('create your languages in the pages directory, for example:'),
+  Pre.View(`
 /pages/
   index.js // english page
   /de/
     index.js // german page
 `),
 
-      h2({ id: 'state' }, 'required state'),
-      p('LanguageSwitch needs to know about the languages in your app.'),
-      Pre.View(`
+  h2({ id: 'state' }, 'required state'),
+  p('LanguageSwitch needs to know about the languages in your app.'),
+  Pre.View(`
 // /assets/app.js
 module.exports = {
   state: {
@@ -62,17 +60,16 @@ module.exports = {
 }
 `),
 
-      p('thats it, your magic app now knows about your languages'),
+  p('thats it, your magic app now knows about your languages'),
 
-      h2({ id: 'source' }, 'source'),
-      p([
-        'the source for this page is in the ',
-        Link(
-          { to: 'https://github.com/magic-modules/languageswitch/tree/master/example' },
-          'example directory',
-        ),
-        ' and gets built and published to github using ',
-        Link({ to: 'https://github.com/magic/core' }, '@magic/core'),
-      ]),
-    ]),
-}
+  h2({ id: 'source' }, 'source'),
+  p([
+    'the source for this page is in the ',
+    Link(
+      { to: 'https://github.com/magic-modules/languageswitch/tree/master/example' },
+      'example directory',
+    ),
+    ' and gets built and published to github using ',
+    Link({ to: 'https://github.com/magic/core' }, '@magic/core'),
+  ]),
+]
