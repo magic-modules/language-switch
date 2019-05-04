7cc4203ba2018ba97a918974aa906a75acf8413e
Jascha <jascha@jaeh.at>
2019-05-05 00:40:04 +0200

    add state.title and state.description

--- example/app.js
+++ example/app.js
@@ -1,6 +1,8 @@
 module.exports = {
   state: {
-    logotext: 'languageswitch',
+    logotext: 'LanguageSwitch',
+    title: '@magic-modules/language-switch',
+    description: '@magic-modules LanguageSwitch. adds an intelligent language switching mechanism to your @magic app.',
     languages: [
       { code: 'en', to: '/', text: 'english' },
       { code: 'de', to: '/de/', text: 'deutsch' },
