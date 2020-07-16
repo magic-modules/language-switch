401bac505f3a7e36d5bac22b195255481db4f80b
jascha <jascha@jaeh.at>
2020-07-16 23:16:30 +0200

    update logotext

--- example/app.mjs
+++ example/app.mjs
@@ -1,10 +1,11 @@
 export const state = {
-  logotext: 'LanguageSwitch',
   title: '@magic-modules/language-switch',
   description: [
     '@magic-modules LanguageSwitch.',
     ' adds an intelligent language switching mechanism to your @magic app.',
   ],
+  logotext: 'LanguageSwitch',
+
   languages: [
     { code: 'en', to: '/', text: 'english' },
     { code: 'de', to: '/de/', text: 'deutsch' },
